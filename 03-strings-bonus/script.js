036bfba8c45018e072a47288ea841c00ade0b970
Johnnyfromtheblock4 <simo.visco4@gmail.com>
2025-06-30 17:42:02 +0200

    [UPD] bonus-3

--- 03-strings-bonus/script.js
+++ 03-strings-bonus/script.js
@@ -16,7 +16,7 @@ console.log(citySubstring); // Output: "York"
 
 // 3. Sostituzione
 // Sostituisci 'York' con 'Delhi'
-const cityReplaced = '';
+const cityReplaced = city.replaceAll("York","Dehli");;
 console.log(cityReplaced); // Output:  "New Delhi"
 
 // 4. Template literal
